e4dfb91005602f213a29e6a67d43990e6892450a
skrahat <asaduzzaman.rahat@mail.mcgill.ca>
2021-04-15 14:48:36 +0600

    first push

---
+++ app/src/main/java/com/example/bondhu/Login.java
@@ -0,0 +1,91 @@
+package com.example.bondhu;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.bondhu.databinding.ActivityLoginBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Login extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    private String user, pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.register.setOnClickListener(v ->
+                startActivity(new Intent(Login.this, Register.class)));
+
+        binding.loginButton.setOnClickListener(v -> {
+            user = binding.username.getText().toString();
+            pass = binding.password.getText().toString();
+
+            if (user.equals("")) {
+                binding.username.setError("can't be blank");
+            } else if (pass.equals("")) {
+                binding.password.setError("can't be blank");
+            } else {
+                String url = "https://bondhu-2021-default-rtdb.firebaseio.com/users.json";
+                final ProgressDialog pd = new ProgressDialog(Login.this);
+                pd.setMessage("Loading...");
+                pd.show();
+
+                StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
+                    if (response.equals("null")) {
+                        Toast.makeText(Login.this, "user not found", Toast.LENGTH_LONG).show();
+                    } else {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+
+                            if (!obj.has(user)) {
+                                Toast.makeText(Login.this, "user not found", Toast.LENGTH_LONG).show();
+                                resetDataAndFocus();
+                            } else if (obj.getJSONObject(user).getString("password").equals(pass)) {
+                                UserDetails.username = user;
+                                UserDetails.password = pass;
+                                startActivity(new Intent(Login.this, Users.class));
+                            } else {
+                                Toast.makeText(Login.this, "incorrect password", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    pd.dismiss();
+                }, volleyError -> {
+                    System.out.println("" + volleyError);
+                    pd.dismiss();
+                });
+
+                RequestQueue rQueue = Volley.newRequestQueue(Login.this);
+                rQueue.add(request);
+            }
+
+        });
+    }
+
+    /*** Reset Data and Focus **/
+    private void resetDataAndFocus() {
+        binding.username.setText("");
+        binding.password.setText("");
+        binding.password.clearFocus();
+        binding.password.clearFocus();
+    }
+}
---
+++ app/src/main/java/com/example/bondhu/Register.java
@@ -0,0 +1,104 @@
+package com.example.bondhu;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.firebase.client.Firebase;
+import com.example.bondhu.databinding.ActivityRegisterBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Register extends AppCompatActivity {
+    private ActivityRegisterBinding binding;
+    private String user, pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Firebase.setAndroidContext(this);
+
+        binding.login.setOnClickListener(v ->
+                startActivity(new Intent(Register.this, Login.class)));
+
+        binding.registerButton.setOnClickListener(v -> {
+            user = binding.username.getText().toString();
+            pass = binding.password.getText().toString();
+
+            if (user.equals("")) {
+                binding.username.setError("can't be blank");
+            } else if (pass.equals("")) {
+                binding.password.setError("can't be blank");
+            } else if (!user.matches("[A-Za-z0-9]+")) {
+                binding.username.setError("only alphabet or number allowed");
+            } else if (user.length() < 5) {
+                binding.username.setError("at least 5 characters long");
+            } else if (pass.length() < 5) {
+                binding.password.setError("at least 5 characters long");
+            } else {
+                String url = "https://bondhu-2021-default-rtdb.firebaseio.com/users.json";
+                final ProgressDialog pd = new ProgressDialog(Register.this);
+                pd.setMessage("Loading...");
+                pd.show();
+
+                StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
+                    Firebase reference = new Firebase("https://bondhu-2021-default-rtdb.firebaseio.com/users");
+
+                    if (response.equals("null")) {
+                        reference.child(user).child("password").setValue(pass);
+                        Toast.makeText(Register.this, "registration successful", Toast.LENGTH_LONG).show();
+                    } else {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+
+                            if (!obj.has(user)) {
+                                reference.child(user).child("password").setValue(pass);
+                                Toast.makeText(Register.this, "registration successful", Toast.LENGTH_LONG).show();
+
+                                startActivity(new Intent(getApplicationContext(), Login.class));
+                                finish();
+                            } else {
+                                Toast.makeText(Register.this, "username already exists", Toast.LENGTH_LONG).show();
+
+                                // reset focus
+                                resetDataAndFocus();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    pd.dismiss();
+                }, volleyError -> {
+                    System.out.println("" + volleyError);
+                    pd.dismiss();
+                });
+
+                RequestQueue rQueue = Volley.newRequestQueue(Register.this);
+                rQueue.add(request);
+            }
+        });
+    }
+
+    /*** Reset Data and Focus **/
+    private void resetDataAndFocus() {
+        binding.username.setText("");
+        binding.password.setText("");
+        binding.password.clearFocus();
+        binding.password.clearFocus();
+    }
+}
