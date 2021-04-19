97d3ac55e727304eb641e422ad0365477ca1993a
rahat <asaduzzaman.rahat@mail.mcgill.ca>
2021-04-20 04:36:43 +0600

    live status completed with friends in real time

--- app/src/main/java/com/example/bondhu/Live.java
+++ app/src/main/java/com/example/bondhu/Live.java
@@ -1,5 +1,6 @@
 package com.example.bondhu;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,8 +23,11 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.firebase.client.Firebase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,17 +35,26 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
 public class Live extends AppCompatActivity {
     ListView usersList;
-    ListView usersList2;
     TextView noUsersText;
     TextView currentUser;
     TextView currentStatusView;
+    TextView friend1;
+    TextView friend2;
+    TextView friend3;
+    TextView friend4;
+    TextView friend5;
     ArrayList<String> al = new ArrayList<>();
     ArrayList<String> al2 = new ArrayList<>();
+    ArrayList<String> friendsArray = new ArrayList<>();
+    ArrayList<String> friendsStatusArray = new ArrayList<>();
+    int totalFriends = 0;
     int totalUsers = 0;
     ProgressDialog pd;
-    Button btnLiveStatusData;
     Button btnLiveStatusData2;
     EditText etLiveStatus;
     DatabaseReference statusDbRef;
@@ -49,33 +62,46 @@ public class Live extends AppCompatActivity {
     DatabaseReference statusDbRef2;
     String userT;
     FloatingActionButton floatingActionButton;
-    FloatingActionButton floatingActionButton2;
-    FloatingActionButton floatingActionButton3;
-    FloatingActionButton floatingActionButton4;
-    FloatingActionButton floatingActionButton5;
+    //FloatingActionButton floatingActionButton2;
+    //FloatingActionButton floatingActionButton3;
+    //FloatingActionButton floatingActionButton4;
+    //FloatingActionButton floatingActionButton5;
     Boolean clicked;
+    String status1;
+    String status2;
+    String status3;
+    String status4;
+    String status5;
+
+    String TAG= "Testing------------xxxx_-----------Testing";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live);
 
         //getting UI IDS
-        usersList = (ListView)findViewById(R.id.usersList);
-        usersList2 = (ListView)findViewById(R.id.usersList2);
         noUsersText = (TextView)findViewById(R.id.noUsersText);
         currentUser = (TextView)findViewById(R.id.currentUser);
+        friend1 = (TextView)findViewById(R.id.friend1);
+        friend2 = (TextView)findViewById(R.id.friend2);
+        friend3 = (TextView)findViewById(R.id.friend3);
+        friend4 = (TextView)findViewById(R.id.friend4);
+        friend5 = (TextView)findViewById(R.id.friend5);
         etLiveStatus = findViewById(R.id.etLiveStatus);
-        btnLiveStatusData = findViewById(R.id.btnLiveStatusData);
         btnLiveStatusData2 = findViewById(R.id.btnLiveStatusData2);
         spinnerStatus = findViewById(R.id.spinnerStatus);
         currentStatusView = (TextView)findViewById(R.id.currentStatusView);
         floatingActionButton = findViewById(R.id.floatingActionButton);
-        floatingActionButton2 = findViewById(R.id.floatingActionButton2);
-        floatingActionButton3 = findViewById(R.id.floatingActionButton3);
-        floatingActionButton4 = findViewById(R.id.floatingActionButton4);
-        floatingActionButton5 = findViewById(R.id.floatingActionButton5);
+        //floatingActionButton2 = findViewById(R.id.floatingActionButton2);
+        //floatingActionButton3 = findViewById(R.id.floatingActionButton3);
+        //floatingActionButton4 = findViewById(R.id.floatingActionButton4);
+        //floatingActionButton5 = findViewById(R.id.floatingActionButton5);
         userT = UserDetails.username;
         clicked = false;
+        GifImageView gifImageView2 = (GifImageView) findViewById(R.id.gifImageView2);
+
 
         statusDbRef = FirebaseDatabase.getInstance().getReference().child("status");
         statusDbRef2 = FirebaseDatabase.getInstance().getReference().child("users");
@@ -93,27 +119,101 @@ public class Live extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinnerStatus.setAdapter(adapter);
 
+///////////////////---------------------construction for status displays--//////////////////////
 
+
+        ////fab button for animation testing... temp////////***construction*****////
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(!clicked){
-                    floatingActionButton2.show();
-                    floatingActionButton3.show();
-                    floatingActionButton4.show();
-                    floatingActionButton5.show();
+                    gifImageView2.setVisibility(View.VISIBLE);
+                    gifImageView2.setImageResource(R.drawable.texting);
                     clicked=true;
                 }else{
-                    floatingActionButton2.hide();
-                    floatingActionButton3.hide();
-                    floatingActionButton4.hide();
-                    floatingActionButton5.hide();
+
+                    gifImageView2.setVisibility(View.VISIBLE);
+                    gifImageView2.setImageResource(R.drawable.gym);
+
                     clicked=false;
                 }
 
             }
         });
+        //gathers friend list
+        // display friends
+        String urlF = "https://bondhu-2021-default-rtdb.firebaseio.com/users.json";
+        //String urlF = "https://bondhu-2021-default-rtdb.firebaseio.com/users/"+UserDetails.username+"/friends.json";
+        StringRequest requestF = new StringRequest(Request.Method.GET, urlF, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String s) {
+                doOnSuccessF(s);
+            }
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                System.out.println("" + volleyError);
+            }
+        });
 
+        RequestQueue rQueueF = Volley.newRequestQueue(Live.this);
+        rQueueF.add(requestF);
+
+        //realtime updating friends name and status
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users");
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                friendsStatusArray.clear();
+                int count = 0;
+                while (friendsArray.size() > count) {
+                    String value = dataSnapshot.child(friendsArray.get(count)).child("currentStatus").getValue(String.class);
+                    friendsStatusArray.add(value);
+                    String w =String.valueOf(count);
+                    Log.i(TAG,w);
+
+                    switch(count+1) {
+                        case 1:
+                            friend1.setText(friendsArray.get(0)+":\n"+friendsStatusArray.get(0));
+                            friend1.setVisibility(View.VISIBLE);
+                            break;
+                        case 2:
+                            friend2.setText(friendsArray.get(1)+":\n"+friendsStatusArray.get(1));
+                            friend2.setVisibility(View.VISIBLE);
+                            break;
+                        case 3:
+                            friend3.setText(friendsArray.get(2)+":\n"+friendsStatusArray.get(2));
+                            friend3.setVisibility(View.VISIBLE);
+                            break;
+                        case 4:
+                            friend4.setText(friendsArray.get(3)+":\n"+friendsStatusArray.get(3));
+                            friend4.setVisibility(View.VISIBLE);
+                            break;
+                        case 5:
+                            friend5.setText(friendsArray.get(4)+":\n"+friendsStatusArray.get(4));
+                            friend5.setVisibility(View.VISIBLE);
+                            break;
+                    }
+                    count++;
+                }
+
+
+
+                //String value = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+///////////////////---------------------construction for status displays--//////////////////////
 
         //update Current Status
         btnLiveStatusData2.setOnClickListener(new View.OnClickListener() {
@@ -135,13 +235,6 @@ public class Live extends AppCompatActivity {
 
 
 
-        //adding liveStatus button
-        btnLiveStatusData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                insertStatusData();
-            }
-        });
         pd = new ProgressDialog(Live.this);
         pd.setMessage("Loading...");
         pd.show();
@@ -166,15 +259,14 @@ public class Live extends AppCompatActivity {
 
 
     }
-
+    //gets users list from database
     public void doOnSuccess(String s){
         try {
             JSONObject obj = new JSONObject(s);
-            String TAG = "livexxxxxxxxx";
 
             Iterator i = obj.keys();
             String key = "";
-            Log.i(TAG, i.toString());
+            //Log.i(TAG, i.toString());
 
             while(i.hasNext()){
                 key = i.next().toString();
@@ -186,22 +278,16 @@ public class Live extends AppCompatActivity {
 
                 totalUsers++;
             }
-            Log.i(TAG, String.valueOf(al2));
-            Log.i(TAG, String.valueOf(al));
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
         if(totalUsers <=1){
             noUsersText.setVisibility(View.VISIBLE);
-            usersList.setVisibility(View.VISIBLE);
         }
         else{
             noUsersText.setVisibility(View.GONE);
-            usersList.setVisibility(View.VISIBLE);
-            usersList2.setVisibility(View.VISIBLE);
-            usersList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al));
-            usersList2.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al2));
 
         }
 
@@ -216,4 +302,89 @@ public class Live extends AppCompatActivity {
 
         Toast.makeText(Live.this,"status updated",Toast.LENGTH_SHORT).show();
     }
+    //generating array of friend
+    public void doOnSuccessF(String s){
+        try {
+            JSONObject obj = new JSONObject(s);
+            JSONObject obj2 = obj.getJSONObject(UserDetails.username).getJSONObject("friends");
+            Iterator i = obj2.keys();
+            String key = "";
+
+            while(i.hasNext()){
+                key = i.next().toString();
+
+                if(!key.equals("123123")) {
+                    friendsArray.add(key);
+                }
+
+                totalFriends++;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if(totalFriends <1){
+
+            //friendsList.setVisibility(View.GONE);
+        }
+        else{
+
+            //friendsList.setVisibility(View.VISIBLE);
+            //friendsList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friendsArray));
+        }
+        // fetching current status from the friendsArray List
+        try{
+            JSONObject obj = new JSONObject(s);
+            Iterator i = obj.keys();
+            String key = "";
+
+            while (i.hasNext()) {
+                key = i.next().toString();
+                int countX = 0;
+                while (friendsArray.size() > countX) {
+
+                    //String w =String.valueOf(friendsArray.size());
+                    //Log.i(TAG,w);
+                    if(key.equals(friendsArray.get(countX))) {
+                        String key2 = obj.getJSONObject(key).getString("currentStatus");
+                        friendsStatusArray.add(key2);
+
+                        //Log.i(TAG,friendsStatusArray.get(countX));
+                        //String z =String.valueOf(countX);
+                        //Log.i(TAG,z);
+                }
+                    countX++;
+                }
+            }
+            for(int z =1;friendsArray.size() +1 >z;z++)
+            switch(z) {
+                case 1:
+                    friend1.setText(friendsArray.get(0)+":\n"+friendsStatusArray.get(0));
+                    friend1.setVisibility(View.VISIBLE);
+                    break;
+                case 2:
+                    friend2.setText(friendsArray.get(1)+":\n"+friendsStatusArray.get(1));
+                    friend2.setVisibility(View.VISIBLE);
+                    break;
+                case 3:
+                    friend3.setText(friendsArray.get(2)+":\n"+friendsStatusArray.get(2));
+                    friend3.setVisibility(View.VISIBLE);
+                    break;
+                case 4:
+                    friend4.setText(friendsArray.get(3)+":\n"+friendsStatusArray.get(3));
+                    friend4.setVisibility(View.VISIBLE);
+                    break;
+                case 5:
+                    friend5.setText(friendsArray.get(4)+":\n"+friendsStatusArray.get(4));
+                    friend5.setVisibility(View.VISIBLE);
+                    break;
+            }
+        } catch (JSONException e) {
+        e.printStackTrace();
+        }
+
+        pd.dismiss();
+    }
+
 }
